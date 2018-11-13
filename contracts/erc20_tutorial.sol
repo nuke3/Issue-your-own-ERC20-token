7fa0902d61bd7595b4159b0b15ad41d7d297e907
Nuke <nuke@PC-Nuke>
2018-11-13 01:33:13 +0100

    minor changes

--- contracts/erc20_tutorial.sol
+++ contracts/erc20_tutorial.sol
@@ -102,7 +102,7 @@ contract Owned {
 // ERC20 Token, with the addition of symbol, name and decimals and assisted
 // token transfers
 // ----------------------------------------------------------------------------
-contract FucksToken is ERC20Interface, Owned, SafeMath {
+contract YourToken is ERC20Interface, Owned, SafeMath {
     string public symbol;
     string public  name;
     uint8 public decimals;
